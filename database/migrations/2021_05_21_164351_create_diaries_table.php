ce647779c2b6697e25a8e3c1aeb437c50928a47c
kazuma.iwabuchi <amuzak4342mittuan@gmail.com>
2021-05-21 09:02:52 +0000

    migration

---
+++ database/migrations/2021_05_21_164351_create_diaries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diaries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('today_event');//"今日の出来事"カラム
+            $table->string('content',500);//"今日の感想"カラム、500文字以内
+            $table->string('tommorow_event');//"明日やる事"カラム
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diaries');
+    }
+}
