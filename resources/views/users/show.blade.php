8a13d6a05e349881f2b27cfb1debc4f3fa08aa6b
kazuma.iwabuchi <amuzak4342mittuan@gmail.com>
2021-05-25 06:22:07 +0000

    modify Mypage

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,6 +3,6 @@
 @section("content")
 <h1>MyPage</h1>
      {!! link_to_route('diaries.create', '日記を書く', [], ['class' => 'btn btn-primary']) !!} {{--投稿フォームリンク--}}
-@include("diaries.index")
+
 
 @endsection
